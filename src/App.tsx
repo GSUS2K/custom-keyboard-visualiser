ad3e844f75ce4824a4ac7b74f381c66237fa10bc
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-05-03 02:41:22 +0530

    Fix React import TS error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './index.css';
 
 // Reusable Audio Context to avoid limits and leaks
